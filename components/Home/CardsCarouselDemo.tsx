08ef03ca7306b7fbc245546a6245e233a74a0c64
41d721 <dav7.digi@yahoo.com>
2024-08-09 10:57:10 +0530

    Added_Hardware-Hackathon

--- components/Home/CardsCarouselDemo.tsx
+++ components/Home/CardsCarouselDemo.tsx
@@ -80,9 +80,9 @@ const data = [
     content: <DummyContent />,
   },
   {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "2019",
+    title: "Hardware Hackathon",
+    src: "/IoT-hardware-hackathon.png",
     content: <DummyContent />,
   },
 ];
